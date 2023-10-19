9c53a754462c0b69cd84123ca32abf948428423e
cherif-ichrak <ichechri@gmail.com>
2023-10-19 08:57:58 +0100

    Projet Murex Club

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ParametrageComponent } from './components/parametrage/parametrage.component';
+import { CompetitionsComponent } from './components/competitions/competitions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Parametrage', component: ParametrageComponent },
+  { path: 'Competition', component: CompetitionsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ParametrageComponent } from './components/parametrage/parametrage.component';
+import { UsersComponent } from './components/users/users.component';
+import { IdentificationComponent } from './components/identification/identification.component';
+import { GroupesComponent } from './components/groupes/groupes.component';
+import { CompetitionsComponent } from './components/competitions/competitions.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MenuComponent, FooterComponent, ParametrageComponent, UsersComponent, IdentificationComponent, GroupesComponent, CompetitionsComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
