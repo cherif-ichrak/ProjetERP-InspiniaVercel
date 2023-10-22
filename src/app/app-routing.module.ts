39366f4b19bed17351e353803f22a9f1325e0158
cherif-ichrak <ichechri@gmail.com>
2023-10-22 22:13:56 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ParametrageComponent } from './components/parametrage/parametrage.component';
 import { CompetitionsComponent } from './components/competitions/competitions.component';
+import { MatchesComponent } from './components/matches/matches.component';
+import { ParametragePwdComponent } from './components/parametrage-pwd/parametrage-pwd.component';
+import { JoueursComponent } from './components/joueurs/joueurs.component';
+import { BiblioComponent } from './components/biblio/biblio.component';
+import { MobiliteComponent } from './components/mobilite/mobilite.component';
+import { EvenementsComponent } from './components/evenements/evenements.component';
+import { ConfigurerGroupeComponent } from './components/configurer-groupe/configurer-groupe.component';
 
 const routes: Routes = [
   { path: 'Parametrage', component: ParametrageComponent },
+  { path: 'ParametragePwd', component: ParametragePwdComponent },
+  {
+    path: 'Parametrage/ConfigurerGroupe',
+    component: ConfigurerGroupeComponent,
+  },
+
   { path: 'Competition', component: CompetitionsComponent },
+  { path: 'Matches', component: MatchesComponent },
+  { path: 'Joueurs', component: JoueursComponent },
+  { path: 'Biblio', component: BiblioComponent },
+  { path: 'Mobilite', component: MobiliteComponent },
+  { path: 'Evenement', component: EvenementsComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,38 @@ import { UsersComponent } from './components/users/users.component';
 import { IdentificationComponent } from './components/identification/identification.component';
 import { GroupesComponent } from './components/groupes/groupes.component';
 import { CompetitionsComponent } from './components/competitions/competitions.component';
+import { MatchesComponent } from './components/matches/matches.component';
+import { ParametragePwdComponent } from './components/parametrage-pwd/parametrage-pwd.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { FormsModule } from '@angular/forms';
+import { JoueursComponent } from './components/joueurs/joueurs.component';
+import { BiblioComponent } from './components/biblio/biblio.component';
+import { MobiliteComponent } from './components/mobilite/mobilite.component';
+import { EvenementsComponent } from './components/evenements/evenements.component';
+import { DetaileJoueurComponent } from './components/detaile-joueur/detaile-joueur.component';
+import { ConfigurerGroupeComponent } from './components/configurer-groupe/configurer-groupe.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent, FooterComponent, ParametrageComponent, UsersComponent, IdentificationComponent, GroupesComponent, CompetitionsComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    ParametrageComponent,
+    UsersComponent,
+    IdentificationComponent,
+    GroupesComponent,
+    CompetitionsComponent,
+    MatchesComponent,
+    ParametragePwdComponent,
+    ContactsComponent,
+    JoueursComponent,
+    BiblioComponent,
+    MobiliteComponent,
+    EvenementsComponent,
+    DetaileJoueurComponent,
+    ConfigurerGroupeComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
